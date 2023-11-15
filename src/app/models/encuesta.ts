4bbae2d74343e2ae4da70d24e058d39a2246a159
JuanTF <117841978+JuanTF@users.noreply.github.com>
2023-11-15 00:05:08 -0500

    Se mejoró el apartado en encuestas y preguntas

--- src/app/models/encuesta.ts
+++ src/app/models/encuesta.ts
@@ -2,7 +2,7 @@ import { Usuarios } from "./usuarios"
 
 export class Encuesta {
     idEncuesta: number = 0
-    valoracionFinal: number = 0
+    nombreEncuesta: string = ""
     comentario: string = ""
     usuario: Usuarios = new Usuarios()
 }
